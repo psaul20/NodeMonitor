567b5afa4eebcb99ef8788b7b4ece9dfd40e09d3
psaul20 <patricksaul20@gmail.com>
2022-01-18 18:27:47 -0600

    Added newlines to email message

--- src/SmsManager/main.py
+++ src/SmsManager/main.py
@@ -24,20 +24,22 @@ def sms_Manager(event, context):
         print("Data not found")
 
 def send_Gmail(message: str):
+    # Add extra lines to avoid CMAEK envelope
+    message = "\r\n\r\n" + message
     # Authenticate gmail using app password
-        auth = (os.getenv("PATRICK_GMAIL_USERNAME"), os.getenv("PATRICK_GMAIL_PASSWORD"))
+    auth = (os.getenv("PATRICK_GMAIL_USERNAME"), os.getenv("PATRICK_GMAIL_PASSWORD"))
 
-        emailPhoneList = list(os.getenv('EMAIL_PHONE_NUMBER_REGISTRY').split(','))
+    emailPhoneList = list(os.getenv('EMAIL_PHONE_NUMBER_REGISTRY').split(','))
 
-        # Establish a secure session with gmail's outgoing SMTP server using your gmail account
-        server = smtplib.SMTP( "smtp.gmail.com", 587 )
-        server.starttls()
-        server.login(auth[0], auth[1])
-        
-        for phone in emailPhoneList:
-            # Send text message through SMS gateway of destination number
-            server.sendmail( auth[0], phone, message)
-            print(f"Message Sent to {phone}")
+    # Establish a secure session with gmail's outgoing SMTP server using your gmail account
+    server = smtplib.SMTP( "smtp.gmail.com", 587 )
+    server.starttls()
+    server.login(auth[0], auth[1])
+    
+    for phone in emailPhoneList:
+        # Send text message through SMS gateway of destination number
+        server.sendmail( auth[0], phone, message)
+        print(f"Message Sent to {phone}")
     
 
 def send_Telegram(message: str):
