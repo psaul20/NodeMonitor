cdaa4307e426afbfb7626aa3f0260e82a8b7d366
psaul20 <patricksaul20@gmail.com>
2022-02-21 15:46:52 -0600

    updated smsManager

--- tests/NodeMonitor_test/PRE_api_test.py
+++ tests/NodeMonitor_test/PRE_api_test.py
@@ -4,10 +4,10 @@ import os
 
 parameters = {
         'stats' : 'true',
-        'start_date' : dt.datetime(2022,1,13,15,8)
+        'start_date' : dt.datetime(2022,1,1,3,51)
     }
 
-response = requests.get(f"https://nodes.presearch.org/api/nodes/status/{os.getenv('PATRICK_PRE_API_KEY')}",
+response = requests.get(f"https://nodes.presearch.org/api/nodes/status/{os.getenv('DANIEL_PRE_API_KEY')}",
         params = parameters)
 responseData = response.json()
 print(responseData)
